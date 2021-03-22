1ee61aa2cfeac389fbede8293afa7c646971f8e0
AgentJ08 <arpit.maheshwari08041999@gmail.com>
2021-03-22 15:42:29 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import styled from 'styled-components'
+import { StyledList, StyledListElem } from './modules/ListStyle';
+
+const StyledNavbar = styled.nav`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    width: 100%;
+    height: 70px;
+    line-height: 70px;
+    background-color: #351F39;   
+`;
+
+const Navbar = () => {
+    return (
+        <StyledNavbar>
+            <StyledList>
+                <StyledListElem>Memer</StyledListElem>
+            </StyledList>
+
+        </StyledNavbar>
+    )
+}
+
+export default Navbar;
